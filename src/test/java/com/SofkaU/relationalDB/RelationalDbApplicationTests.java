d4ddaffc2bc50ba86d206bbc9a68549a0a84a890
David Ernesto Reina Munar <davidereina@hotmail.com>
2022-05-01 22:05:57 -0500

    Project created successfully

---
+++ src/test/java/com/SofkaU/relationalDB/RelationalDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SofkaU.relationalDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationalDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
